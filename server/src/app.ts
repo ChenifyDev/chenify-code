3172f893ebaa9a4d9d0bdfa3f6a845d4f2136dda
chen3283891376 <3283891376@qq.com>
2026-08-27 11:54:30 +0800

    fix: 忘记改allowedOrigins了

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,7 +14,7 @@ import { routes as oauthRoutes } from "./oauth";
 
 const app = new Hono();
 
-const allowedOrigins = ["https://code.chenify.top", "http://localhost:5173"];
+const allowedOrigins = ["https://hub.chenify.top", "http://localhost:5173"];
 
 app.use(
     cors({
